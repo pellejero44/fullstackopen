1fe9e29f82ff6fad2e6cf2ec7003a7da88972daf
RED\apellejero <apellejero@alfatecsistemas.es>
2021-02-01 11:35:42 +0100

    exercise one

--- part1-react/src/Message.js
+++
@@ -1,5 +0,0 @@
-const Message = (props) =>{
-  return <h1>{props.message}</h1>
-}
-
-export default Message;
--- part1-react/src/index.js
+++ part1-react/src/index.js
@@ -1,15 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.content.name} {props.content.exercises}
+      </p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <div>
+        <Part content={props.content[0]} />
+        <Part content={props.content[1]} />
+        <Part content={props.content[2]} />
+      </div>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises{' '}
+        {props.total[0].exercises +
+          props.total[1].exercises +
+          props.total[2].exercises}
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content content={course.parts} />
+      <Total total={course.parts} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
