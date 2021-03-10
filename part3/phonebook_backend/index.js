c909c6ba079c9ce8caee30a0aa5649c7cbe53e94
Angel Pellejero <apellejero@alfatecsistemas.es>
2021-03-10 16:58:43 +0100

    Exercises 3.7.-3.8.

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
 
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body);
+})
+
+const app = express();
 app.use(express.json());
+app.use(morgan(':method :url - :body'));
 
 let persons = [
   {
