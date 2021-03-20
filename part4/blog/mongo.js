50d211a6a48f663ddd30576d6e7a8f368ee535b1
Angel Pellejero <apellejero@alfatecsistemas.es>
2021-03-20 14:58:46 +0100

    Exercises 4.3.-4.7.

--- part4/blog/mongo.js
+++ part4/blog/mongo.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const {MONGO_DB_URI, MONGO_DB_URI_TEST, NODE_ENV} = process.env;
 
-const connectionString = process.env.MONGO_DB_URI;
+const connectionString = NODE_ENV === 'test' ? MONGO_DB_URI_TEST : MONGO_DB_URI;
+console.log('connectionString', NODE_ENV)
 
 mongoose
   .connect(connectionString, {
