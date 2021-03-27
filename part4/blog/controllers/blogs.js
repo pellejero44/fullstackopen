df1524b7229ef73a5699af9c76bef578ee137099
Angel Pellejero <apellejero@alfatecsistemas.es>
2021-03-27 15:03:28 +0100

    4.17: bloglist expansion, step5

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -2,7 +2,12 @@ const blogsRouter = require('express').Router();
 const Blog = require('../models/Blog');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog
+    .find({})
+    .populate('user', {
+      username: 1,
+      name:1,
+    });
   response.json(blogs);
 });
 
