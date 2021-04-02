00be4eba80f0f8dc733f5cc50004f55ce36dcdc7
Angel Pellejero <apellejero@alfatecsistemas.es>
2021-04-02 11:28:35 +0200

    5.7* Blog list frontend, step7

--- part5/front/src/components/Blog.js
+++ part5/front/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react';
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+  const showWhenVisible = { display: visible ? '' : 'none' };
 
-export default Blog
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleVisibility = () => setVisible(!visible);
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisibility}>{visible ? 'hide' : 'View'}</button>
+      <div style={showWhenVisible}>
+        <a href={blog.url}>{blog.url}</a>
+        <br />
+        {blog.likes} likes
+        <button>like</button>
+        <br />
+        {blog.user?.name}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
