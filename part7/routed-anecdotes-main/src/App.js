8f28616530041aa929004982a77b6a95426e5399
Angel Pellejero <apellejero@alfatecsistemas.es>
2021-05-01 13:57:18 +0200

    7.4: anecdotes and hooks step1

--- part7/routed-anecdotes-main/src/App.js
+++ part7/routed-anecdotes-main/src/App.js
@@ -5,6 +5,7 @@ import {
   Link,
   useHistory,
 } from 'react-router-dom';
+import { useField } from './hooks';
 
 const Menu = () => {
   const padding = {
@@ -88,20 +89,20 @@ const Footer = () => (
 
 const CreateNew = (props) => {
   const history = useHistory();
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
+  const content = useField({ type: 'text' });
+  const author = useField({ type: 'text' });
+  const info = useField({ type: 'text' });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
 
-    props.setNotification(`a new anecdote ${content} created`);
+    props.setNotification(`a new anecdote ${content.value} created`);
     history.push('/');
     setTimeout(() => {
       props.setNotification('');
@@ -114,27 +115,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name='content'
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name='author'
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name='info'
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
@@ -207,6 +196,9 @@ const App = () => {
             <Anecdote anecdote={anecdoteById(match.params.id)} />
           )}
         />
+        <Route>
+          <h1>Not Found</h1>
+        </Route>
         <Footer />
       </Router>
     </div>
